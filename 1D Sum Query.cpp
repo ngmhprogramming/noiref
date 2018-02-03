2dccc173c82972b705c2fe818d07846a6bc5000a
ngmh <Ngmhcartoon@gmail.com>
2018-02-03 19:43:45 +0800

    Some Graph Theory and DP Stuff

---
+++ 1D Sum Query.cpp
@@ -0,0 +1,7 @@
+int numbers[n], sums[n];
+int query(int start, int end){ //start and end inclusive, 0-indexed
+	return sums[end] - sums[start-1];
+}
+//int main()
+sums[0] = numbers[0]; //base case, because it is 0-indexed
+for(int i = 1; i < n; i++) sums[i] = sums[i-1]+numbers[i]; // precomputation
