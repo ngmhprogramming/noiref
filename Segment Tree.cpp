ec0ea61df82d7e90b7a6c655b7a83b24f6fb833f
ngmh <Ngmhcartoon@gmail.com>
2018-02-09 10:44:14 +0800

    Order of functions

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -14,12 +14,13 @@ struct node {
 		if(P <= middle) left->point_update(P, V);
 		value = min(left->value, right->value);
 	}
-	int range_query(int A, int B){ //query range from A to B inclusive
-		point_query(); //lazy propogation only
-		if(start == A && end == B) return value;
-		if(A > middle) return right->range_query(A, B);
-		if(B <= middle) return left->range_query(A, B);
-		return min(left->range_query(A, middle), right->range_query(middle+1, B)); //in this case this segment tree stores the minimum
+	void range_update(int X, int Y, int change){ //lazy propogation only
+		if(start == X && end == Y) lazy += change;
+		else {
+			if(X > middle) right->range_update(X, Y, change);
+			else if(Y <= middle) left->range_update(X, Y, change);
+			value = min(left->point_query(), right->point_query());
+		}
 	}
 	int point_query(){ //lazy propogation only
 		if(start == end){ value += lazy; lazy = 0; return value;}
@@ -28,12 +29,11 @@ struct node {
 		lazy = 0;
 		return value;
 	}
-	void range_update(int X, int Y, int change){ //lazy propogation only
-		if(start == X && end == Y) lazy += change;
-		else {
-			if(X > middle) right->range_update(X, Y, change);
-			else if(Y <= middle) left->range_update(X, Y, change);
-			value = min(left->point_query(), right->point_query());
-		}
+	int range_query(int A, int B){ //query range from A to B inclusive
+		point_query(); //lazy propogation only
+		if(start == A && end == B) return value;
+		if(A > middle) return right->range_query(A, B);
+		if(B <= middle) return left->range_query(A, B);
+		return min(left->range_query(A, middle), right->range_query(middle+1, B)); //in this case this segment tree stores the minimum
 	}
 } *root;
