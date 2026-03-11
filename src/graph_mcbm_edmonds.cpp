20a0a711cabee4d334fbb6a51a3eca2654fe9282
ngmh <ngmhcartoon@gmail.com>
2026-03-11 13:26:50 +0800

    improve edmonds implementation

--- src/graph_mcbm_edmonds.cpp
+++ src/graph_mcbm_edmonds.cpp
@@ -2,85 +2,153 @@ class Edmonds {
 private:
     int n, m;
     int mcbm = 0;
-    vector<int> mate, p, d, bl;
-    vector<vector<int>> g;
+    vector<int> mate;
     vector<vector<int>> b;
+    vector<int> p, d, bl;
+    vector<vector<int>> g;
 
-    int find_lca(int x, int y, vector<int>& p, vector<int>& bl,
-				 const vector<int>& mate) {
-        vector<bool> used(n, false);
+    void match(int u, int v) {
+        g[u][v] = g[v][u] = -1;
+        mate[u] = v;
+        mate[v] = u;
+    }
+
+    vector<int> trace(int x) {
+        vector<int> vx;
         while (true) {
-            x = bl[x];
-            used[x] = true;
-            if (mate[x] == -1) break;
-            x = p[mate[x]];
+            while (bl[x] != x) x = bl[x];
+            if (!vx.empty() && vx.back() == x) break;
+            vx.push_back(x);
+            x = p[x];
         }
-        while (true) {
-            y = bl[y];
-            if (used[y]) return y;
-            y = p[mate[y]];
+        return vx;
+    }
+
+    void contract(int c, int x, int y, vector<int>& vx, vector<int>& vy) {
+        b[c].clear();
+        int r = vx.back();
+        while (!vx.empty() && !vy.empty() && vx.back() == vy.back()) {
+            r = vx.back();
+            vx.pop_back();
+            vy.pop_back();
+        }
+        b[c].push_back(r);
+        b[c].insert(b[c].end(), vx.rbegin(), vx.rend());
+        b[c].insert(b[c].end(), vy.begin(), vy.end());
+        for (int i = 0; i <= c; i++) {
+            g[c][i] = g[i][c] = -1;
+        }
+        for (int z : b[c]) {
+            bl[z] = c;
+            for (int i = 0; i < c; i++) {
+                if (g[z][i] != -1) {
+                    g[c][i] = z;
+                    g[i][c] = g[i][z];
+                }
+            }
         }
     }
 
-    void update_path(int x, int y, int lca, vector<int>& p, vector<int>& d,
-					 vector<int>& bl, queue<int>& Q, const vector<int>& mate) {
-        while (bl[x] != lca) {
-            p[x] = y;
-            y = mate[x];
-            if (d[y] == 1) { d[y] = 0; Q.push(y); }
-            if (bl[x] == x) bl[x] = lca;
-            if (bl[y] == y) bl[y] = lca;
-            x = p[y];
+    vector<int> lift(vector<int>& vx) {
+        vector<int> A;
+        while (vx.size() >= 2) {
+            int z = vx.back(); vx.pop_back();
+            if (z < n) {
+                A.push_back(z);
+                continue;
+            }
+            int w = vx.back();
+            int i = (A.size() % 2 == 0 ?
+                find(b[z].begin(), b[z].end(), g[z][w]) - b[z].begin() : 0);
+            int j = (A.size() % 2 == 1 ?
+                find(b[z].begin(), b[z].end(), g[z][A.back()]) - b[z].begin() : 0);
+            int k = b[z].size();
+            int dif = (A.size() % 2 == 0 ? i % 2 == 1 : j % 2 == 0) ? 1 : k - 1;
+            while (i != j) {
+                vx.push_back(b[z][i]);
+                i = (i + dif) % k;
+            }
+            vx.push_back(b[z][i]);
         }
+        return A;
     }
 
 public:
-    Edmonds(int n) : n(n), m(n + n / 2), mate(n, -1), p(n), d(n),
-					 bl(n), g(n, vector<int>(n, -1)) {}
+    Edmonds(int n) : n(n) {
+        m = n + n / 2;
+        mate.assign(n, -1);
+        b.resize(m);
+        p.resize(m);
+        d.resize(m);
+        bl.resize(m);
+        g.assign(m, vector<int>(m, -1));
+    }
 
     void addEdge(int u, int v) {
-        g[u][v] = g[v][u] = 1;
+        g[u][v] = u;
+        g[v][u] = v;
     }
 
     int compute() {
-        mcbm = 0;
         fill(mate.begin(), mate.end(), -1);
-        for (int i = 0; i < n; ++i) {
-            if (mate[i] == -1) {
-                queue<int> Q;
-                Q.push(i);
-                fill(p.begin(), p.end(), -1);
-                fill(d.begin(), d.end(), -1);
-                iota(bl.begin(), bl.end(), 0);
-                d[i] = 0;
-
-                while (!Q.empty()) {
-                    int u = Q.front(); Q.pop();
-                    for (int v = 0; v < n; ++v) {
-                        if (g[u][v] == -1 || bl[u] == bl[v]) continue;
-                        if (d[v] == -1) {
-                            d[v] = 1; p[v] = u;
-                            if (mate[v] == -1) {
-                                while (v != -1) {
-                                    int pv = p[v];
-                                    int ppv = mate[pv];
-                                    mate[v] = pv; mate[pv] = v;
-                                    v = ppv;
+        
+        for (int ans = 0; ; ans++) {
+            fill(d.begin(), d.end(), 0);
+            queue<int> Q;
+            for (int i = 0; i < m; i++) bl[i] = i;
+            for (int i = 0; i < n; i++) {
+                if (mate[i] == -1) {
+                    Q.push(i);
+                    p[i] = i;
+                    d[i] = 1;
+                }
+            }
+            int c = n;
+            bool aug = false;
+            while (!Q.empty() && !aug) {
+                int x = Q.front(); Q.pop();
+                if (bl[x] != x) continue;
+                for (int y = 0; y < c; y++) {
+                    if (bl[y] == y && g[x][y] != -1) {
+                        if (d[y] == 0) {
+                            p[y] = x;
+                            d[y] = 2;
+                            p[mate[y]] = y;
+                            d[mate[y]] = 1;
+                            Q.push(mate[y]);
+                        } else if (d[y] == 1) {
+                            vector<int> vx = trace(x);
+                            vector<int> vy = trace(y);
+                            if (vx.back() == vy.back()) {
+                                contract(c, x, y, vx, vy);
+                                Q.push(c);
+                                p[c] = p[b[c][0]];
+                                d[c] = 1;
+                                c++;
+                            } else {
+                                aug = true;
+                                vx.insert(vx.begin(), y);
+                                vy.insert(vy.begin(), x);
+                                vector<int> A = lift(vx);
+                                vector<int> B = lift(vy);
+                                A.insert(A.end(), B.rbegin(), B.rend());
+                                for (int i = 0; i < (int)A.size(); i += 2) {
+                                    match(A[i], A[i + 1]);
+                                    if (i + 2 < (int)A.size()) {
+                                        addEdge(A[i + 1], A[i + 2]);
+                                    }
                                 }
-                                mcbm++; goto next_i;
                             }
-                            d[mate[v]] = 0; Q.push(mate[v]);
-                        } else if (d[v] == 0) {
-                            int lca = find_lca(u, v, p, bl, mate);
-                            update_path(u, v, lca, p, d, bl, Q, mate);
-                            update_path(v, u, lca, p, d, bl, Q, mate);
+                            break;
                         }
                     }
                 }
             }
-            next_i:;
+            if (!aug) {
+                mcbm = ans;
+                return mcbm;
+            }
         }
-        return mcbm;
     }
 
     int getMCBM() { return mcbm; }
@@ -90,4 +158,5 @@ public:
 Edmonds solver(N);
 solver.addEdge(u, v);
 solver.compute();
+solver.getMCBM();
 solver.getMatched(i);
